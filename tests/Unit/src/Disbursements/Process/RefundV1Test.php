7f461f851d72e580cece1a89f99fb986eb432801
Jibin Joseph <jibin.joseph@fingent.com>
2023-03-07 09:20:56 +0530

    Disbursement unit testing scripts

---
+++ tests/Unit/src/Disbursements/Process/RefundV1Test.php
@@ -0,0 +1,47 @@
+<?php
+
+use momopsdk\Common\Constants\MobileMoney;
+use momopsdk\Common\Process\BaseProcess;
+use momopsdkTest\Unit\src\Common\Process\ProcessTestCase;
+use momopsdkTest\Unit\src\mocks\MockResponse;
+use momopsdk\Disbursement\Models\RefundModel;
+use momopsdk\Disbursement\Models\ResponseModel;
+use momopsdk\Disbursement\Process\CreateRefundV1;
+
+
+
+class RefundV1Test extends ProcessTestCase
+{
+    protected function setUp(): void
+    {
+        $oReqDataObject = new RefundModel();
+
+        $oReqDataObject
+                    ->setAmount('2000')
+                    ->setCurrency('EUR')
+                    ->setExternalId('12345678')
+                    ->setPayerMessage('Payer message here')
+                    ->setPayeeNote('Payee note here')
+                    ->setReferenceIdToRefund('ce20fe55-fc5c-4a50-8d5a-43a85e67f928');
+        $callbackUrl = "http://webhook.site/c84cd23c-062b-49bb-b206-909bc8625207";
+
+
+        $env = parse_ini_file(__DIR__ . './../../../../../config.env');
+        $sSubsKey = $env['disbursement_subscription_key'];
+        $sTargetEnvironmentlter = $env['target_environment'];
+        $subType = 'disbursement';
+        $this->constructorArgs = [$oReqDataObject, $sSubsKey, $sTargetEnvironmentlter, $callbackUrl, $subType];
+        $this->requestMethod = 'POST';
+        $this->requestUrl =
+            MobileMoney::getBaseUrl() .
+            '/disbursement/v1_0/refund';
+        $this->processType = BaseProcess::ASYNCHRONOUS_PROCESS;
+        $this->className = CreateRefundV1::class;
+        $this->reqObj = $this->instantiateClass(
+            $this->className,
+            $this->constructorArgs
+        );
+        $this->mockResponseObject = 'RefundV1.json';
+        $this->responseType = ResponseModel::class;
+    }
+}
