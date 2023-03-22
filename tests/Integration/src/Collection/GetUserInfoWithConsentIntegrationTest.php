0091566636a79c6c7c4821f15424d934d36cd35f
sandra.vijayan <sandra.vijayan@fingent.com>
2023-03-22 10:49:37 +0530

    GetUserInfoWithConsentIntegrationTest updated

--- tests/Integration/src/Collection/GetUserInfoWithConsentIntegrationTest.php
+++ tests/Integration/src/Collection/GetUserInfoWithConsentIntegrationTest.php
@@ -3,6 +3,7 @@
 use momopsdk\Collection\Collection;
 use momopsdk\Common\Models\UserDetail;
 use momopsdk\Common\Process\BaseProcess;
+use momopsdk\Common\Constants\MobileMoney;
 use momopsdk\Common\Process\GetUserInfoWithConsent;
 use momopsdkTest\Integration\src\IntegrationTestCase;
 
@@ -26,6 +27,10 @@ class GetUserInfoWithConsentIntegrationTest extends IntegrationTestCase
     protected function setUp(): void
     {
         $env = parse_ini_file(__DIR__ . './../../../../config.env');
+        $bcAuthorizeFormData = "login_hint=ID:msisdn/msisdn&scope=profile&access_type=online";
+        $Oauth2TokenFormData = "grant_type=urn:openid:params:grant-type:ciba&auth_req_id={auth_req_id}";
+        MobileMoney::setBcAuthorizeFormData($bcAuthorizeFormData);
+        MobileMoney::setOauth2TokenFormData($Oauth2TokenFormData);
         $this->request = Collection::getUserInfoWithConsent(
             $env['collection_subscription_key'],
             $env['target_environment']
